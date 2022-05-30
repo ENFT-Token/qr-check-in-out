7e3170b3d4c31bb7e28ebc55f37c0b537d55e410
Long9725 <develop9725@gmail.com>
2022-05-31 00:06:43 +0900

    UI

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'dart:io';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_checkinout/constant.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:http/http.dart' as http;
 import 'package:jwt_decode/jwt_decode.dart';
@@ -15,14 +16,15 @@ import 'KlipLoginButton.dart';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-
 class User {
   int status;
   String accessToken;
   String address;
   String place;
+
   User(this.status, this.accessToken, this.address, this.place);
 }
+
 AudioCache audioPlayer = AudioCache();
 
 Future<String> CheckInOut(User user, addrToken) async {
@@ -30,15 +32,14 @@ Future<String> CheckInOut(User user, addrToken) async {
     print(addrToken);
     if (Jwt.isExpired(addrToken) == true) {
       // 기간 만료
-      throw "Invalid NFT Token: 30초가 지난 티켓입니다.";
+      throw "30초가 지난 티켓입니다.";
     }
     Map<String, dynamic> jsonValue = Jwt.parseJwt(addrToken);
     print(jsonValue);
     if (!jsonValue.containsKey('address') ||
         !jsonValue.containsKey('nftToken')) {
-      throw "Invalid NFT Token: 올바르지 않은 QR 형식입니다.";
+      throw "올바르지 않은 QR 형식입니다.";
     }
-
 
     print("hh ${jsonValue['nftToken']}");
     Map<String, dynamic> payload = Jwt.parseJwt(jsonValue['nftToken']);
@@ -46,11 +47,11 @@ Future<String> CheckInOut(User user, addrToken) async {
     print(user.place);
     print(payload['place']);
     if (user.place != payload['place']) {
-      throw "Invalid NFT Token: 이용권의 위치와 사용하려는 위치가 일치하지 않습니다.";
+      throw "이용권의 위치와 사용하려는 위치가 일치하지 않습니다.";
     }
     if (Jwt.isExpired(jsonValue['nftToken']) == true) {
       // 기간 만료
-      throw "Invalid NFT Token: 기간 만료";
+      throw "기간이 만료된 토큰입니다";
     }
     var checkUrl = Uri.parse('http://13.209.200.101:3000/check');
     var response = await http.post(checkUrl, body: {
@@ -60,34 +61,32 @@ Future<String> CheckInOut(User user, addrToken) async {
       'Authorization': 'Bearer ' + user.accessToken,
     });
     Map<String, dynamic> body = jsonDecode(response.body);
-    if(response.statusCode == 201) {
+    if (response.statusCode == 201) {
       audioPlayer.play(body["status"] + ".mp3");
       print(body["status"]); // "checkin" or "checkout"
       print(body["place"]);
       return "[ " + body["place"] + " ] " + body["status"];
-    }
-    else {
+    } else {
       print(body);
-      throw "Invalid NFT Token: 유효하지 않은 티켓입니다.";
+      throw "유효하지 않은 티켓입니다.";
     }
-  }
-  catch(e) {
+  } catch (e) {
+    print(e);
     return e.toString();
   }
-
 }
 
 Future<User> Login(address) async {
   var loginUrl = Uri.parse('http://13.209.200.101:3000/auth/admin/login');
   var response = await http.post(loginUrl, body: {'address': address});
   print(response.body);
-  if(response.statusCode == 201) {
+  if (response.statusCode == 201) {
     final body = jsonDecode(response.body);
-    return User(response.statusCode, body['access_token'], body['address'],body['place']);
+    return User(response.statusCode, body['access_token'], body['address'],
+        body['place']);
   }
-  return User(response.statusCode , "", "", "");
+  return User(response.statusCode, "", "", "");
 }
-
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -109,9 +108,11 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(kDefaultPadding * 3),
         child: Column(
-            mainAxisAlignment:MainAxisAlignment.spaceAround,
-            children: <Widget>[
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
             KlipLoginButton(
               onPressed: () async {
                 final klipApi = KlipAPi();
@@ -135,25 +136,27 @@ class _LoginPageState extends State<LoginPage> {
                 if (address != "") {
                   print(address);
                   final user = await Login(address);
-                  if(user.status != 201)
-                    FlutterDialog("안내","로그인 실패");
+                  if (user.status != 201)
+                    FlutterDialog("안내", "로그인 실패");
                   else {
-                    Navigator.push(context, MaterialPageRoute(
-                        builder: (context) => CheckInOutQRView(user: user)));
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                CheckInOutQRView(user: user)));
                   }
                 } else {
-                  FlutterDialog("안내","클립 연결 실패");
+                  FlutterDialog("안내", "클립 연결 실패");
                 }
               },
             ),
           ],
         ),
-      ),
+      )),
     );
   }
 
-
-  void FlutterDialog(title,content) {
+  void FlutterDialog(title, content) {
     showDialog(
         context: context,
         //barrierDismissible - Dialog를 제외한 다른 화면 터치 x
@@ -194,7 +197,8 @@ class _LoginPageState extends State<LoginPage> {
 
 class CheckInOutQRView extends StatefulWidget {
   final User user;
-  const CheckInOutQRView({Key ?key, required this.user}) : super(key: key);
+
+  const CheckInOutQRView({Key? key, required this.user}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _QRViewState();
@@ -205,6 +209,7 @@ class _QRViewState extends State<CheckInOutQRView> {
   QRViewController? controller;
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   bool scanAllow = true;
+
   // In order to get hot reload to work we need to pause the camera if the platform
   // is android, or resume the camera if the platform is iOS.
   @override
@@ -217,18 +222,21 @@ class _QRViewState extends State<CheckInOutQRView> {
   }
 
   void Toast(content) {
+    print(content);
+    bool isError = (content.contains("checkin") || content.contains("checkout"))
+        ? true
+        : false;
+    print(isError);
     final snackBar = SnackBar(
-
       content: Container(
-        height: 20,
-        child: Text(content),
-      ),
-      action: SnackBarAction(
-        label: 'Undo',
-        onPressed: () {
-          // Some code to undo the change.
-        },
-      ),
+          height: 100,
+          child: Center(
+            child: RichText(
+                text: TextSpan(
+                    text: content,
+                    style:
+                        TextStyle(color: isError ? Colors.white : Colors.red, fontSize: 20))),
+          )),
     );
 
     // Find the ScaffoldMessenger in the widget tree
@@ -238,81 +246,33 @@ class _QRViewState extends State<CheckInOutQRView> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Expanded(flex: 4, child: _buildQrView(context)),
-          Expanded(
-            flex: 1,
-            child: FittedBox(
-              fit: BoxFit.contain,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  if (result != null)
-                    Text(
-                        'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
-                  else
-                    Text(widget.user.accessToken),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        margin: const EdgeInsets.all(8),
-                        child: ElevatedButton(
-                            onPressed: () async {
-                              await controller?.toggleFlash();
-                              setState(() {});
-                            },
-                            child: FutureBuilder(
-                              future: controller?.getFlashStatus(),
-                              builder: (context, snapshot) {
-                                return Text('Flash: ${snapshot.data}');
-                              },
-                            )),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.all(8),
-                        child: ElevatedButton(
-                            onPressed: () async {
-                              await controller?.flipCamera();
-                              setState(() {});
-                            },
-                            child: FutureBuilder(
-                              future: controller?.getCameraInfo(),
-                              builder: (context, snapshot) {
-                                if (snapshot.data != null) {
-                                  return Text(
-                                      'Camera facing ${describeEnum(snapshot.data!)}');
-                                } else {
-                                  return const Text('loading');
-                                }
-                              },
-                            )),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.all(8),
-                        child: ElevatedButton(
-                            onPressed: () async {
-                              final result = await CheckInOut(widget.user, '{"address":"0xb438de8ac7be89f5f65dcd9d17a5029ee050edf7","nftToken":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwbGFjZSI6IkVORlQg7Zes7Iqk7J6lIiwic3RhcnRfZGF0ZSI6IjIwMjItMDQtMTMiLCJlbmRfZGF0ZSI6IjIwMjItMDQtMTQiLCJpYXQiOjE2NDk4MTU0NTIsImV4cCI6MTY0OTkwMTg1Mn0._GJXMPmDrugviQPgJTpL5Dz6nMAB4goIpbL4WuIyHUc"}');
-                              Toast(result);
-                            },
-                            child: FutureBuilder(
-                              builder: (context, snapshot) {
-                                  return Text('TEST CHECKOUT');
-                              },
-                            )),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
-    );
+        body: Stack(alignment: Alignment.bottomCenter, children: <Widget>[
+      _buildQrView(context),
+      Positioned(
+          bottom: size.height * 0.1,
+          child: FutureBuilder(
+            future: controller?.getCameraInfo(),
+            builder: (context, snapshot) {
+              if (snapshot.data != null) {
+                return IconButton(
+                    icon: Icon(
+                      Icons.cameraswitch_rounded,
+                      size: 66,
+                      color: Colors.white,
+                    ),
+                    onPressed: () async {
+                      await controller?.flipCamera();
+                      setState(() {});
+                    });
+              } else {
+                print(snapshot.error);
+                return const Text('loading');
+              }
+            },
+          ))
+    ]));
   }
 
   Widget _buildQrView(BuildContext context) {
@@ -336,13 +296,12 @@ class _QRViewState extends State<CheckInOutQRView> {
     );
   }
 
-
   void _onQRViewCreated(QRViewController controller) {
     setState(() {
       this.controller = controller;
     });
     controller.scannedDataStream.listen((scanData) async {
-      if(scanAllow == true && scanData.code != null) {
+      if (scanAllow == true && scanData.code != null) {
         scanAllow = false;
         Timer(Duration(seconds: 5), () {
           scanAllow = true;
